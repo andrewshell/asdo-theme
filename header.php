61f65084c5677276d7ff3777e3ca1d0dae189b32
Andrew Shell <andrew@andrewshell.org>
2026-03-14 09:33:19 -0500

    Updating various favicon images and paths

--- header.php
+++ header.php
@@ -20,13 +20,13 @@
 
 	<link rel="alternate" href="<?php echo esc_url( get_bloginfo( 'rss2_url' ) ); ?>" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>">
 
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-	<link rel="manifest" href="/site.webmanifest">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_theme_file_uri( 'img/apple-touch-icon.png' ) ); ?>">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( get_theme_file_uri( 'img/favicon-32x32.png' ) ); ?>">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( get_theme_file_uri( 'img/favicon-16x16.png' ) ); ?>">
+	<link rel="manifest" href="<?php echo esc_url( get_theme_file_uri( 'img/site.webmanifest' ) ); ?>">
 
 	<link rel="webmention" href="https://webmention.io/blog.andrewshell.org/webmention" />
-	<link rel="alternate" type="application/activity+json" href="https://fed.brid.gy/r/<?php echo rawurlencode( get_permalink() ); ?>">
+	<link rel="alternate" type="application/activity+json" href="https://fed.brid.gy/r/https%3A%2F%2Fblog.andrewshell.org%2F">
 
 	<?php wp_head(); ?>
 </head>
